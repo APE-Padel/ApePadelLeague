845751246e7b9b9f929f4368cfa822f6667e6209
PauCasademont <u1953627@campus.udg.edu>
2025-12-21 11:27:55 +0100

    Export app correclty for vercel

--- backend/api/index.js
+++ backend/api/index.js
@@ -3,21 +3,18 @@ import dotenv from "dotenv";
 import routes from "./routes.js";
 
 dotenv.config();
-startServer();
 
-function startServer() {
-    const app = express();
+const app = express();
 
-    app.use(express.json());
-    app.use("/", routes);
+app.use(express.json());
+app.use("/", routes);
 
-    if (!process.env.VERCEL) {
-        const PORT = process.env.PORT || 3000;
-        app.listen(PORT, () => {
-            console.log(`Server running on http://localhost:${PORT}`);
-        });
-    }
-
-    module.exports = app;
+// Local only
+if (!process.env.VERCEL) {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
 }
 
+export default app;
