49577fce3f9dc0ef0cd1116a8869578f6e1aacac
PauCasademont <u1953627@campus.udg.edu>
2026-02-06 22:41:41 +0100

    Add update match results endpoint

--- backend/data/matchesClient.js
+++ backend/data/matchesClient.js
@@ -9,8 +9,26 @@ export async function getMatches(filters = {}) {
     return result;
 }
 
+export async function getMatchById(matchId) {
+    const result = await Match.findById(matchId);
+    if (!result) {
+        throw new Error("Match not found");
+    }    
+    return result;
+}
+
 export async function createMatch(matchProps) {
     const match = new Match(matchProps);
     const result = await match.save();
     return result;
+}
+
+export async function updateMatchById(matchId, updateProps) {
+  const updatedMatch = await Match.findByIdAndUpdate(
+    matchId,
+    updateProps,
+    { new: true }
+  );
+
+  return updatedMatch;
 }
--- backend/services/matchesService.js
+++ backend/services/matchesService.js
@@ -1,5 +1,8 @@
+import mongoose from "mongoose";
 import * as seasonsClient from "../data/seasonsClient.js";
 import * as matchesClient from "../data/matchesClient.js";
+import * as standingsService from "./standingsService.js";
+import { MATCH_STATUS } from "../constants.js";
 
 export async function getActiveSeasonMatches() {
     const activeSeasons = await seasonsClient.getSeasons({ isActive: true });
@@ -10,4 +13,31 @@ export async function getActiveSeasonMatches() {
     const activeSeason = activeSeasons[0];
     const matches = await matchesClient.getMatches({ season: activeSeason._id });
     return matches;
+}
+
+export async function updateMatchResult(matchId, homeScore, awayScore) {
+    const match = await matchesClient.getMatchById(matchId);
+    const seasonId = match.season;
+
+    const matchStatus = homeScore == 0 && awayScore == 0 ? MATCH_STATUS.SCHEDULED : MATCH_STATUS.COMPLETED;
+    const updateProps = {
+        "home.score": homeScore,
+        "away.score": awayScore,
+        status: matchStatus
+    };
+    
+    const session = await mongoose.startSession();
+
+    try{
+        session.startTransaction();
+        await matchesClient.updateMatchById(matchId, updateProps);
+        await standingsService.recalculateStandings(seasonId);
+
+        await session.commitTransaction();
+    } catch (error) {
+        await session.abortTransaction();
+        throw error;
+    } finally {
+        session.endSession();
+    }
 }
