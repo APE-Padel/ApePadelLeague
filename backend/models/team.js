fb42ee2d43c36f61ba59ea9a599ac436007b0dcd
PauCasademont <u1953627@campus.udg.edu>
2026-02-15 21:29:09 +0100

    Add teams page

--- backend/models/team.js
+++ backend/models/team.js
@@ -2,7 +2,11 @@ import mongoose from 'mongoose';
 
 const teamSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
-  court: String,
+  court: {
+    name: { type: String, required: true },
+    locationUrl: { type: String, required: true },
+    indoor: { type: Boolean, required: true }
+  },  
   logoBase64: String,
   players: [String],
 }, { timestamps: true }); 
